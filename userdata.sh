efc085fcd8ceeacbd792e68167eddd1e0318f32c
HristoDimitrovv <hristo.dimitrovv1@gmail.com>
2023-02-26 10:34:32 +0200

    changes

--- userdata.sh
+++ userdata.sh
@@ -4,4 +4,4 @@ yum update -y upgrade -y
 yum install -y httpd php php-mysqlnd mysql git&&
 systemctl enable --now httpd
 git clone https://github.com/HristoDimitrovv/web-app &&
-cp -v swo-bgapp/web/* /var/www/html/
+cp -v web-app/web/* /var/www/html/
